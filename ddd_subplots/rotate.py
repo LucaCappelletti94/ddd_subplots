5e6c05b8afd90ff4be095a9dfd28ae91264070bf
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-23 15:47:50 +0200

    Refactored rotation method

--- ddd_subplots/rotate.py
+++ ddd_subplots/rotate.py
@@ -1,10 +1,12 @@
+"""Package to produce rotating 3d plots."""
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Union
 from tqdm.auto import tqdm
 import os
 from sklearn.preprocessing import MinMaxScaler
 from multiprocessing import Pool, cpu_count
+from environments_utils import is_macos
 import imageio
 from pygifsicle import optimize
 import shutil
@@ -12,11 +14,16 @@ import shutil
 
 def rotate_z(x: np.ndarray, y: np.ndarray, z: np.ndarray, theta: float):
     w = x+1j*y
-    return np.real(np.exp(1j*theta)*w)/np.sqrt(2), np.imag(np.exp(1j*theta)*w)/np.sqrt(2), z/np.sqrt(2)
+    return np.vstack([
+        np.real(np.exp(1j*theta)*w)/np.sqrt(2),
+        np.imag(np.exp(1j*theta)*w)/np.sqrt(2),
+        z/np.sqrt(2)
+    ])
 
 
-def _job(func: Callable, xs: np.ndarray, ys: np.ndarray, zs: np.ndarray, theta: float, args, kwargs, path):
-    fig, axes = func(*rotate_z(xs, ys, zs, theta), *args, **kwargs)
+def _job(func: Callable, points: np.ndarray, theta: float, args, kwargs, path):
+    fig, axes = func(
+        rotate_z(points[0], points[1], points[2], theta), *args, **kwargs)
     for axis in axes.flatten():
         axis.set_xticklabels([])
         axis.set_yticklabels([])
@@ -32,26 +39,52 @@ def _job_wrapper(task: Tuple):
     _job(*task)
 
 
-def rotate(func: Callable, xs: np.ndarray, ys: np.ndarray, zs: np.ndarray, path: str, fps: int = 24, duration: int = 1, cache_directory: str = ".rotate", parallelize: bool = True, verbose: bool = False, *args, **kwargs):
+def rotate(
+    func: Callable,
+    points: np.ndarray,
+    path: str,
+    *args,
+    fps: int = 24,
+    duration: int = 1,
+    cache_directory: str = ".rotate",
+    parallelize: bool = True,
+    verbose: bool = False,
+    **kwargs
+):
     """Create rotating gif of given image.
-        func: Callable, function return the figure.
-        xs: np.ndarray, x coordinates.
-        ys: np.ndarray, y coordinates.
-        zs: np.ndarray, z coordinates.
-        path: str, path where to save the GIF.
-        fps: int = 24, number of FPS to create.
-        duration: int = 1, duration of the rotation in seconds.
-        cache_directory: str = ".rotate", directory where to store the frame.
-        parallelize: bool = True, whetever to parallelize execution.
-        verbose: bool = False, whetever to be verbose about frame creation.
-        *args, positional arguments to be passed to the `func` callable.
-        **kwargs, keyword argument to be passed to the `func` callable
+
+    Parameters
+    -----------------------
+    func: Callable,
+        function return the figure.
+    points: np.ndarray,
+        The 3D array to rotate.
+    path: str,
+        path where to save the GIF.
+    *args,
+        positional arguments to be passed to the `func` callable.
+    fps: int = 24,
+        number of FPS to create.
+    duration: int = 1,
+        duration of the rotation in seconds.
+    cache_directory: str = ".rotate",
+        directory where to store the frame.
+    parallelize: bool = True,
+        whetever to parallelize execution.
+    verbose: bool = False,
+        whetever to be verbose about frame creation.
+    **kwargs,
+        keyword argument to be passed to the `func` callable
+
     """
     os.makedirs(cache_directory, exist_ok=True)
-    X = MinMaxScaler(feature_range=(-1, 1)).fit_transform(np.array([xs, ys, zs]).T).T
+    X = MinMaxScaler(
+        feature_range=(-1, 1)
+    ).fit_transform(points.T).T
+
     tasks = [
         (
-            func, *X, 2*np.pi * frame / (duration*fps), args, kwargs,
+            func, X, 2*np.pi * frame / (duration*fps), args, kwargs,
             "{cache_directory}/{frame}.jpg".format(
                 cache_directory=cache_directory,
                 frame=frame
@@ -66,7 +99,8 @@ def rotate(func: Callable, xs: np.ndarray, ys: np.ndarray, zs: np.ndarray, path:
                 p.imap(_job_wrapper, tasks),
                 total=len(tasks),
                 desc="Rendering frames",
-                disable=not verbose))
+                disable=not verbose
+            ))
             p.close()
             p.join()
     else:
--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -2,14 +2,15 @@ from ddd_subplots import subplots, rotate
 import numpy as np
 from sklearn import datasets
 from sklearn.decomposition import PCA
+from environments_utils import is_macos
 
 
-def my_func(xs: np.ndarray, ys: np.ndarray, zs: np.ndarray, *args, **kwargs):
+def my_func(points: np.ndarray, *args, **kwargs):
     fig, axes = subplots(1, 3, figsize=(9, 3))
     axs = axes.flatten()
-    axs[0].scatter(xs, ys, zs, **kwargs)
-    axs[1].scatter(ys, zs, xs, **kwargs)
-    axs[2].scatter(zs, xs, ys, **kwargs)
+    axs[0].scatter(points[0], points[1], points[2], **kwargs)
+    axs[1].scatter(points[1], points[2], points[0], **kwargs)
+    axs[2].scatter(points[2], points[0], points[1], **kwargs)
     fig.tight_layout()
     return fig, axes
 
@@ -18,7 +19,8 @@ def test_rotate():
     X, y = datasets.load_iris(return_X_y=True)
     X_reduced = PCA(n_components=3).fit_transform(X)
     colors = np.array(["red", "green", "blue"])[y]
-    rotate(my_func, *X_reduced.T, path="test.gif",
+    rotate(my_func, X_reduced.T, path="test.gif",
            duration=3, fps=10, verbose=True, parallelize=False, c=colors, marker='o', s=20)
-    rotate(my_func, *X_reduced.T, path="test.gif",
-           duration=3, fps=10, c=colors, marker='o', s=20)
+    if not is_macos():
+        rotate(my_func, X_reduced.T, path="test.gif",
+               duration=3, fps=10, c=colors, marker='o', s=20)
