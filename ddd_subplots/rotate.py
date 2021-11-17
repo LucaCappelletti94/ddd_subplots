cbb5d8c7d045b5922003218cb6c358c09593da06
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-11-17 21:03:32 +0100

    Fixed kwarg collision

--- ddd_subplots/rotate.py
+++ ddd_subplots/rotate.py
@@ -65,7 +65,7 @@ def rotating_spiral(*features: List[np.ndarray], theta: float) -> np.ndarray:
     Numpy array with rotated values.
     """
     for _ in range(len(features)):
-        new_features = rotate_along_last_axis(*features, theta)
+        new_features = rotate_along_last_axis(*features, theta=theta)
         features[-1] = new_features[0]
         features[:-1] = new_features[1:]
     return np.vstack(features)
