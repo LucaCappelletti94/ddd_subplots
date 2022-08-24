01485a206f00f3f32b87ecafb18c3321bb6839c7
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-24 23:12:41 +0200

    Ypdated gitignore

--- build/lib/ddd_subplots/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module providing utilities to animate rotations of multidimensional objects."""
-from .subplots import subplots
-from .rotate import rotate
-from .utils import display_video_at_path
-
-__all__ = ["subplots", "rotate", "display_video_at_path"]
--- build/lib/ddd_subplots/__version__.py
+++
@@ -1,2 +0,0 @@
-"""Current version of package ddd_subplots"""
-__version__ = "1.0.19"
