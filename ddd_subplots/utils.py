02091b1b6c5c39367e35bc11b1406d462186a06c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-09-01 11:45:15 +0200

    Handled corner case

--- ddd_subplots/utils.py
+++ ddd_subplots/utils.py
@@ -1,15 +1,16 @@
+from typing import Optional
 import os
 from environments_utils import is_notebook
 
 
-def display_video_at_path(path: str, height: int = 480, width: int = 480):
+def display_video_at_path(path: str, width: str = "480", height: Optional[str] = "480"):
     """Returns IPython object to properly display video at given path.
 
     Parameters
     -------------------------
-    path: str,
-    height: int = 480,
-    width: int = 480
+    path: str
+    width: str = "480"
+    height: Optional[str] = "480"
     """
     if not os.path.exists(path):
         raise ValueError(
@@ -20,7 +21,7 @@ def display_video_at_path(path: str, height: int = 480, width: int = 480):
         return HTML(
             (
                 "<center>"
-                "<video width=\"{width}\" height=\"{height}\" src=\"{path}\" autoplay muted playsinline loop>"
+                "<video width=\"{width}\"{height} src=\"{path}\" autoplay muted playsinline loop>"
                 "<source src=\"{path}\" type=\"video/{extension}\" />"
                 "Your browser does not support the video tag."
                 "</video>"
@@ -28,7 +29,9 @@ def display_video_at_path(path: str, height: int = 480, width: int = 480):
             ).format(
                 path=path,
                 width=width,
-                height=height,
+                height="" if height is None else " height=\"{height}\"".format(
+                    height=height
+                ),
                 extension=path.split(".")[-1]
             )
         )
